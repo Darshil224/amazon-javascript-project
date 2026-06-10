7f77e8e37c0d7cd9590d8b6e96fda3651767bb70
Darshil224 <darshilchitranshi2204@gmail.com>
2026-06-10 17:18:12 +0530

    use product.js file

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,28 +1,4 @@
-const products = [{
-    image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-    name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5,
-        count: 87
-    },
-    priceCents: 1090
-}, {
-    image: 'images/products/intermediate-composite-basketball.jpg',
-    name: 'Intermediate Size Basketball',
-    rating: {
-        stars: 4,
-        count: 127
-    },
-    priceCents: 2095
-},{
-    image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5,
-        count: 56
-    },
-    priceCents: 799
-}];
+//we are getting the product array from the products.js file which is loaded before this script file. this file is loaded after the product.js file, which is necessary because we need products array created first before looping in it.
 
 let productsHTML='';
 products.forEach((product)=>{
