8d3aa29f85c4cb8fdb1f8a9cdbd4278d68b34733
Darshil224 <darshilchitranshi2204@gmail.com>
2026-06-19 14:07:32 +0530

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,22 +1,27 @@
-export let cart= JSON.parse(localStorage.getItem('cart'));
+export let cart;
+loadFromStorage();
 
-if(!cart){
+export function loadFromStorage(){
+        cart= JSON.parse(localStorage.getItem('cart'));
+
+        if(!cart){
 
 
-    cart = [{
-            productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-            quantity: 2,
-            deliveryOptionId: '1'
-        },
-        {
-            productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-            quantity: 1,
-            deliveryOptionId: '2'
+            cart = [{
+                    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+                    quantity: 2,
+                    deliveryOptionId: '1'
+                },
+                {
+                    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+                    quantity: 1,
+                    deliveryOptionId: '2'
+                }
+                ]; 
+                // An empty array ([]) is truthy in JavaScript, so if the user removes all items,
+                // cart becomes [] (not null), and this default cart is not assigned.
+                
         }
-        ]; 
-        // An empty array ([]) is truthy in JavaScript, so if the user removes all items,
-        // cart becomes [] (not null), and this default cart is not assigned.
-        
 }
 
 function saveToStorage(){
